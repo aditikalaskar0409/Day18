bdabf1d14e67068431f1f631bfb57768c454687b
Aditi Kalaskar <80143404+aditikalaskar0409@users.noreply.github.com>
2022-01-08 02:45:38 -0800

    Add files via upload

---
+++ DeckOfCardMain.java
@@ -0,0 +1,10 @@
+public class DeckOfCardMain {
+    public static void main(String[] args) {
+        
+       
+        DeckOfCards deckOfCardsGame = new DeckOfCards();
+        deckOfCardsGame.deckOfCards();
+        deckOfCardsGame.players();
+    }
+}
+
